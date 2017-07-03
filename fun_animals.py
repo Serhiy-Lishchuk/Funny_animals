53f485e21d66003c0299ccf5bf2d9fa93b2f8a73
Serhiy <mcseruy@gmail.com>
2017-07-03 22:35:54 +0300

    add function initialized animal

--- fun_animals.py
+++ fun_animals.py
@@ -4,7 +4,8 @@ import random
 MAX_X = 1366
 MAX_Y = 768
 MAX_ANIMALS = 100
-SHOW_SIZE = 64
+ANIMAL_SIZE = 64
+animals = []
 
 
 class Animal():
@@ -15,21 +16,28 @@ class Animal():
         self.img_num = random.randint(1, 4)
         self.image_filename = "animal" + str(self.img_num) + ".png"
         self.image = pygame.image.load(self.image_filename).convert()
-        self.image = pygame.transform.scale(self.image(SHOW_SIZE, SHOW_SIZE))
+        self.image = pygame.transform.scale(self.image(ANIMAL_SIZE, ANIMAL_SIZE))
 
     def move_animal(self):
         self.y = self.y + self.speed
         if self.y > MAX_Y:
-            self.y = 0 - SHOW_SIZE
+            self.y = 0 - ANIMAL_SIZE
         i = random.randint(1, 3)
         if i == 1:
             self.x += 1
             if self.x > MAX_X:
-                self.x = 0 - SHOW_SIZE
+                self.x = 0 - ANIMAL_SIZE
         elif i == 2:
             self.x -= 1
-            if self.x < 0 - SHOW_SIZE:
+            if self.x < 0 - ANIMAL_SIZE:
                 self.x = MAX_X
 
     def draw_animal(self):
         screen.blit(self.image, (self.x, self.y))
+
+
+def initialized_animal(max_animals, animals):
+    for a in range(0, max_animals):
+        x = random.randint(0, MAX_X)
+        y = random.randint(0, MAX_Y)
+        animals.append(Animal(x, y))
